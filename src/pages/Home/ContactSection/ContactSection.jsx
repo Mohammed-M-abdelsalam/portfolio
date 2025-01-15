fe678bb4c8c19eff75c0519be7e8c7e632d29c1f
Mohamed <mo.magdyabdelsalam@gmail.com>
2025-01-15 20:24:42 +0200

    adding whatsapp in contacts

--- src/pages/Home/ContactSection/ContactSection.jsx
+++ src/pages/Home/ContactSection/ContactSection.jsx
@@ -6,12 +6,14 @@ function ContactSection() {
                 <div className="contact">
                     <h2> CONTACT </h2>
                     <a href='mailto:mo.magdyabdelsalam@gmail.com'>mo.magdyabdelsalam@gmail.com</a>
+                    <a href="tel:01008840871">01008840871</a>
                 </div>
                 <div className="socials">
                     <h2> SOCIALS </h2>
                     <a href='https://github.com/Mohammed-M-abdelsalam' target="_blank">github</a>
+                    <a href='https://www.linkedin.com/in/mohammed-m-abdelsalam/' target="_blank">linkedin</a>
+                    <a href='https://wa.me/01008840871' target="_blank">whatsapp</a>
                     <br />
-                    <a href='' target="_blank">linkedin</a>
                 </div>
             </div>
         </section>
