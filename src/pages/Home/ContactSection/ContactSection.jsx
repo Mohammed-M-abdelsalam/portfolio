1812a1063059a1a1bbe3781032a8518bdbcdb918
Mohamed <mo.magdyabdelsalam@gmail.com>
2025-01-15 20:29:52 +0200

    fixing phone number

--- src/pages/Home/ContactSection/ContactSection.jsx
+++ src/pages/Home/ContactSection/ContactSection.jsx
@@ -6,13 +6,13 @@ function ContactSection() {
                 <div className="contact">
                     <h2> CONTACT </h2>
                     <a href='mailto:mo.magdyabdelsalam@gmail.com'>mo.magdyabdelsalam@gmail.com</a>
-                    <a href="tel:01008840871">01008840871</a>
+                    <a href="tel:201008840871">01008840871</a>
                 </div>
                 <div className="socials">
                     <h2> SOCIALS </h2>
                     <a href='https://github.com/Mohammed-M-abdelsalam' target="_blank">github</a>
                     <a href='https://www.linkedin.com/in/mohammed-m-abdelsalam/' target="_blank">linkedin</a>
-                    <a href='https://wa.me/01008840871' target="_blank">whatsapp</a>
+                    <a href='https://wa.me/201008840871' target="_blank">whatsapp</a>
                     <br />
                 </div>
             </div>
